6d962b73df0ea2a3fa9eaad9c65328ae24e730e8
Masashi <ma.orange381@gmail.com>
2023-03-17 09:36:57 -0700

    hamberger menu in header3

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-// const contactBtn = document.querySelector("#formBtn");
-// contactBtn.addEventListener('click',() => {
-//     const text = document.contactName.yourname.value;
-//     const Email = document.contactName.yourmail.value;
-//     const Subject = document.contactName.subject.value;
-//     const message = document.contactName.comment.value;
+const contactBtn = document.querySelector("#formBtn");
+contactBtn.addEventListener('click',() => {
+    const text = document.contactName.yourname.value;
+    const Email = document.contactName.yourmail.value;
+    const Subject = document.contactName.subject.value;
+    const message = document.contactName.comment.value;
 
-//     if (text.length === 0) {
-//         alert('Please fill in your name');
-//         return false;
-//     }
-//     if (Email.length === 0) {
-//         alert('Please fill in an email address');
-//         return false;
-//     }
-//     if (Subject.length === 0) {
-//         alert('Please fill in your subject');
-//         return false;
-//     }
-//     if (message.length === 0) {
-//         alert('please fill in your comment');
-//         return false;
-//     }
-// });
+    if (text.length === 0) {
+        alert('Please fill in your name');
+        return false;
+    }
+    if (Email.length === 0) {
+        alert('Please fill in an email address');
+        return false;
+    }
+    if (Subject.length === 0) {
+        alert('Please fill in your subject');
+        return false;
+    }
+    if (message.length === 0) {
+        alert('please fill in your comment');
+        return false;
+    }
+});
 
 const MenuContainer = document.querySelector('.menu-container');
 const MenuOpen = document.querySelector('.open');
